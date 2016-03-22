fca323d4c26eb09b033836b516f37d769321e8b4
debugMonster <nntmac@yahoo.com>
2016-03-21 17:48:48 -0700

    cc

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // get all the tools we need
 var express  = require('express');
 var app      = express();
-var port     = process.env.PORT || 3000;
+var port     = process.env.PORT || 3001;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
@@ -37,7 +37,7 @@ io.on('connection' , function (socket){
     });
 });
 
-server.listen(3000);
+server.listen(3002);
 
 
 
